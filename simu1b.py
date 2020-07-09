f12c312b17f9224705097cc3345a529c47f380fc
EggPool <dev@eggpool.net>
2020-07-09 08:15:08 +0200

    Temp data for current lottery bias analysis

---
+++ simu1b.py
@@ -0,0 +1,23 @@
+"""
+Many draws on a single "nodes" file, to analyse odds and bias.
+"""
+
+
+from libs.utils import random_hash
+from libs.utils import linear_ip_score4, shuffle
+from libs.nodesreader import NodesReader
+
+
+if __name__ == "__main__":
+    reader = NodesReader("NODES/nodes.1")
+    # save_whois()
+
+    for test in range(100000):
+        cycle_hash = random_hash()
+        # print("Run {}".format(test))
+        winner = reader.winner(cycle_hash)
+        ip, ip_class = reader.verifiers[winner][:2]
+        ip_class_count = reader.ip_classes[ip_class][0]
+        print("{},{},{}".format(ip, ip_class, ip_class_count))
+
+
